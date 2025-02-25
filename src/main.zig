066e361fc3038d6f9f9295ba32147420d74cad61
Rui-Carrilho <rpcarrilho5+github@gmail.com>
2025-02-25 00:34:20 +0000

    more vm stuff

--- src/main.zig
+++ src/main.zig
@@ -2,12 +2,15 @@ const std = @import("std");
 const memory = @import("memory.zig");
 const chunk = @import("chunk.zig");
 const debug = @import("debug.zig");
-
+const VM = @import("vm.zig");
 
 pub fn main() !void {
     // Allocate memory
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     var allocator = gpa.allocator();
+    defer gpa.deinit();
+
+    VM.initVM();
 
     var newChunk: chunk.Chunk = undefined;
     chunk.initChunk(&newChunk);
@@ -15,7 +18,10 @@ pub fn main() !void {
     try chunk.writeChunk(&newChunk, @intFromEnum(chunk.OpCode.OP_CONSTANT), 123, &allocator);
     try chunk.writeChunk(&newChunk, @intCast(constant), 123, &allocator);
     try chunk.writeChunk(&newChunk, @intFromEnum(chunk.OpCode.OP_RETURN), 123, &allocator);
+    
     debug.disassembleChunk(&newChunk, "test chunk");
+    
+    VM.freeVM();
     chunk.freeChunk(&newChunk, &allocator);
 }
 
--- src/vm.zig
+++ src/vm.zig
@@ -1,6 +1,44 @@
 const std = @import("std");
 const Chunk = @import("chunk.zig");
 
-pub const VM = struct {
-    chunk: *Chunk.Chunk
-};;
+pub const VM = struct { 
+    chunk: *Chunk.Chunk,
+    ip: [] u8
+};
+
+var vm: VM = undefined;
+
+pub const InterpretResult = enum { 
+    INTERPRET_OK, 
+    INTERPRET_COMPILE_ERROR, 
+    INTERPRET_RUNTIME_ERROR 
+};
+
+pub fn initVM() void {}
+
+pub fn freeVM() void {}
+
+pub fn run() InterpretResult {
+    while(true) {
+        const instruction = readByte();
+        switch (instruction) {
+            @intFromEnum(Chunk.OpCode.OP_RETURN) => {
+                return InterpretResult.INTERPRET_OK;
+            },
+            else => unreachable,
+        }
+    }
+
+}
+
+fn readByte() u8 {
+    const byte = vm.ip[0];
+    vm.ip += 1;
+    return byte;
+}
+
+pub fn interpret(chunk: *Chunk.Chunk) InterpretResult {
+    vm.chunk = chunk;
+    vm.ip = chunk.code;
+    return run();
+}
