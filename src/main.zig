431b1cc9667a78663d96ab7c5d0ad108ba26e4f5
Rui-Carrilho <rpcarrilho5+github@gmail.com>
2025-03-08 00:55:04 +0000

    more groundwork for scanner

--- src/main.zig
+++ src/main.zig
@@ -10,11 +10,23 @@ pub fn main() !void {
     var allocator = gpa.allocator();
     //defer _ = gpa.deinit();
 
+    const args = std.os.argv;
+    const argc = args.len;
+
     VM.initVM();
+
+    if (argc == 1) {
+        try repl();
+    } else if (argc == 2) {
+        try runFile(args[1]);
+    } else {
+        std.debug.print("Usage: clox [path]\n", .{});
+        std.process.exit(64);
+    }
 
     var newChunk: chunk.Chunk = undefined;
     chunk.initChunk(&newChunk);
-    var constant: usize = try chunk.addConstant(&newChunk, 3 , &allocator);
+    var constant: usize = try chunk.addConstant(&newChunk, 3, &allocator);
     try chunk.writeChunk(&newChunk, @intFromEnum(chunk.OpCode.OP_CONSTANT), 123, &allocator);
     try chunk.writeChunk(&newChunk, @intCast(constant), 123, &allocator);
 
@@ -32,14 +44,35 @@ pub fn main() !void {
 
     //try chunk.writeChunk(&newChunk, @intFromEnum(chunk.OpCode.OP_NEGATE), 123, &allocator);
     try chunk.writeChunk(&newChunk, @intFromEnum(chunk.OpCode.OP_RETURN), 123, &allocator);
-    
+
     debug.disassembleChunk(&newChunk, "test chunk");
     _ = VM.interpret(&newChunk);
-    
+
     VM.freeVM();
     chunk.freeChunk(&newChunk, &allocator);
 }
 
-test "simple test" {
+pub fn repl() !void {
+    var line: [1024]u8 = undefined;
+    
+    while (true) {
+        std.debug.print("> ", .{});
 
+        const input = std.io.getStdIn().reader().readUntilDelimiterOrEof(
+            &line,
+            '\n'
+        ) catch |err| {
+            std.debug.print("\n", .{});
+            return err;
+        };
+
+        if (input == null) {
+            std.debug.print("\n", .{});
+            break;
+        }
+
+        try interpret(input.?);
+    }
 }
+
+test "simple test" {}
