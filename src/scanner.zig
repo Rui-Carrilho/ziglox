b7776056a7922c903eaa0ce74b9b6671ee4decb5
Rui-Carrilho <rpcarrilho5+github@gmail.com>
2025-03-13 01:13:25 +0000

    more groundwork

--- src/scanner.zig
+++ src/scanner.zig
@@ -1,42 +1,59 @@
 const std = @import("std");
 
-pub const Scanner = struct {
-    start: []const u8,
-    current: []const u8,
-    line: i8
-};
+pub const Scanner = struct { start: []const u8, current: []const u8, line: i8 };
 
 pub const TokenType = enum {
     // Single-character tokens.
-    TOKEN_LEFT_PAREN, TOKEN_RIGHT_PAREN,
-    TOKEN_LEFT_BRACE, TOKEN_RIGHT_BRACE,
-    TOKEN_COMMA, TOKEN_DOT, TOKEN_MINUS, TOKEN_PLUS,
-    TOKEN_SEMICOLON, TOKEN_SLASH, TOKEN_STAR,
+    TOKEN_LEFT_PAREN,
+    TOKEN_RIGHT_PAREN,
+    TOKEN_LEFT_BRACE,
+    TOKEN_RIGHT_BRACE,
+    TOKEN_COMMA,
+    TOKEN_DOT,
+    TOKEN_MINUS,
+    TOKEN_PLUS,
+    TOKEN_SEMICOLON,
+    TOKEN_SLASH,
+    TOKEN_STAR,
 
     // One or two character tokens.
-    TOKEN_BANG, TOKEN_BANG_EQUAL,
-    TOKEN_EQUAL, TOKEN_EQUAL_EQUAL,
-    TOKEN_GREATER, TOKEN_GREATER_EQUAL,
-    TOKEN_LESS, TOKEN_LESS_EQUAL,
+    TOKEN_BANG,
+    TOKEN_BANG_EQUAL,
+    TOKEN_EQUAL,
+    TOKEN_EQUAL_EQUAL,
+    TOKEN_GREATER,
+    TOKEN_GREATER_EQUAL,
+    TOKEN_LESS,
+    TOKEN_LESS_EQUAL,
 
     // Literals.
-    TOKEN_IDENTIFIER, TOKEN_STRING, TOKEN_NUMBER,
-    
+    TOKEN_IDENTIFIER,
+    TOKEN_STRING,
+    TOKEN_NUMBER,
+
     // Keywords.
-    TOKEN_AND, TOKEN_CLASS, TOKEN_ELSE, TOKEN_FALSE,
-    TOKEN_FOR, TOKEN_FUN, TOKEN_IF, TOKEN_NIL, TOKEN_OR,
-    TOKEN_PRINT, TOKEN_RETURN, TOKEN_SUPER, TOKEN_THIS,
-    TOKEN_TRUE, TOKEN_VAR, TOKEN_WHILE,
+    TOKEN_AND,
+    TOKEN_CLASS,
+    TOKEN_ELSE,
+    TOKEN_FALSE,
+    TOKEN_FOR,
+    TOKEN_FUN,
+    TOKEN_IF,
+    TOKEN_NIL,
+    TOKEN_OR,
+    TOKEN_PRINT,
+    TOKEN_RETURN,
+    TOKEN_SUPER,
+    TOKEN_THIS,
+    TOKEN_TRUE,
+    TOKEN_VAR,
+    TOKEN_WHILE,
 
-    TOKEN_ERROR, TOKEN_EOF
+    TOKEN_ERROR,
+    TOKEN_EOF,
 };
 
-pub const Token = struct {
-    type: TokenType,
-    start: []const u8,
-    length: i32,
-    line: i32
-};
+pub const Token = struct { type: TokenType, start: []const u8, length: i32, line: i32 };
 
 var scanner: Scanner = undefined;
 
@@ -48,13 +65,34 @@ pub fn initScanner(source: []const u8) void {
 
 pub fn scanToken() Token {
     scanner.start = scanner.current;
-    if(isAtEnd()) return makeToken(TokenType.TOKEN_EOF);
-    
+    if (isAtEnd()) return makeToken(TokenType.TOKEN_EOF);
+
+    var c: u8 = advance();
+
+    switch (c) {
+        '(' => return makeToken(TokenType.TOKEN_LEFT_PAREN),
+        ')' => return makeToken(TokenType.TOKEN_LEFT_PAREN),
+        '{' => return makeToken(TokenType.TOKEN_LEFT_BRACE),
+        '}' => return makeToken(TokenType.TOKEN_RIGHT_BRACE),
+        ';' => return makeToken(TokenType.TOKEN_SEMICOLON),
+        ',' => return makeToken(TokenType.TOKEN_COMMA),
+        '.' => return makeToken(TokenType.TOKEN_DOT),
+        '-' => return makeToken(TokenType.TOKEN_MINUS),
+        '+' => return makeToken(TokenType.TOKEN_PLUS),
+        '/' => return makeToken(TokenType.TOKEN_SLASH),
+        '*' => return makeToken(TokenType.TOKEN_STAR),
+    }
+
     return errorToken("Unexpected character");
 }
 
 pub fn isAtEnd() bool {
     return scanner.current.len == 0;
+}
+
+pub fn advance() u8 {
+    scanner.current += 1;
+    return scanner.current[-1];
 }
 
 pub fn makeToken(tokenType: TokenType) Token {
