1305466255be4c14a1ff67663143f53414be0f95
Rui-Carrilho <rpcarrilho5+github@gmail.com>
2025-04-03 01:07:08 +0100

    groundwork laid out

--- src/value.zig
+++ src/value.zig
@@ -1,31 +1,36 @@
 const std = @import("std");
 const memory = @import("memory.zig");
 
-pub const ValueArray = struct {
-    capacity: usize,
-    count: usize,
-    values: []Value
+pub const ValueArray = struct { capacity: usize, count: usize, values: []Value };
+
+pub const ValueType = enum { VAL_BOOL, VAL_NIL, VAL_NUMBER };
+
+pub const Value = struct { type: ValueType, as: union { boolean: bool, number: f64 } };
+
+pub fn BOOL_VAL(value: bool) Value {
+    return Value{
+        .type = ValueType.VAL_BOOL,
+        .as = .{ .boolean = value },
+    };
+}
+
+pub const NIL_VAL: Value = Value{
+    .type = ValueType.VAL_NIL,
+    .as = .{ .number = 0 },
 };
 
-pub const ValueType = enum {
-    VAL_BOOL,
-    VAL_NIL,
-    VAL_NUMBER
-};
-
-pub const Value = struct {
-    type: ValueType,
-    as: union{
-        boolean: bool,
-        number: f64
-    }
-};
+pub fn NUMBER_VAL(value: f64) Value {
+    return Value{
+        .type = ValueType.VAL_NUMBER,
+        .as = .{ .number = value },
+    };
+}
 
 pub fn initValueArray(array: *ValueArray) void {
     array.values = memory.initArray(Value);
     array.capacity = 0;
     array.count = 0;
-} 
+}
 
 pub fn writeValueArray(array: *ValueArray, value: Value, allocator: *std.mem.Allocator) !void {
     if (array.capacity < array.count + 1) {
