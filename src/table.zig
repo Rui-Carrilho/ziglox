397f5ad76acc42f66c064972f8a807377931a321
Rui-Carrilho <rpcarrilho5+github@gmail.com>
2025-05-13 00:41:19 +0100

    added table getter

--- src/table.zig
+++ src/table.zig
@@ -6,15 +6,9 @@ const Allocator = @import("allocator.zig");
 
 const TABLE_MAX_LOAD = 0.75;
 
-pub const Table = struct {
-    count: usize,
-    entries: ?[]Entry
-};
+pub const Table = struct { count: usize, entries: ?[]Entry };
 
-pub const Entry = struct {
-    key: ?*Object.ObjString,
-    value: Value.Value
-};
+pub const Entry = struct { key: ?*Object.ObjString, value: Value.Value };
 
 pub fn initTable(table: *Table) void {
     table.count = 0;
@@ -42,13 +36,13 @@ pub fn adjustCapacity(table: *Table, capacity: usize) void {
     var entries = memory.ALLOCATE(Entry, capacity);
     var i: usize = 0;
 
-    while(i < capacity) : (i = i + 1) {
+    while (i < capacity) : (i = i + 1) {
         entries[i].key = null;
         entries[i].value = null;
     }
 
     i = 0;
-    while(i < capacity) : (i = i + 1) {
+    while (i < capacity) : (i = i + 1) {
         const entry = table.entries[i];
         if (entry.key == null) continue;
 
@@ -83,10 +77,20 @@ pub fn tableSet(table: *Table, key: *Object.ObjString, value: Value.Value) bool 
 pub fn tableAddAll(from: *Table, to: *Table) void {
     var i: usize = 0;
 
-    while(i < from.entries.?.len) : (i = i + 1) {
+    while (i < from.entries.?.len) : (i = i + 1) {
         const entry = from.entries[i];
         if (entry.key != null) {
             tableSet(to, entry.key, entry.value);
         }
     }
+}
+
+pub fn tableGet(table: *Table, key: *Object.ObjString, value: *Value.Value) bool {
+    if (table.count == 0) return false;
+
+    const entry = findEntry(table.entries, key);
+    if (entry.key.? == null) return false;
+
+    value.* = entry.value;
+    return true;
 }
