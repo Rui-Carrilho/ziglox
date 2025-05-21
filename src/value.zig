9e340a55a345c0c54678e3fa46ac40b734247ef9
Rui-Carrilho <rpcarrilho5+github@gmail.com>
2025-05-22 00:11:18 +0100

    comment

--- src/value.zig
+++ src/value.zig
@@ -125,4 +125,6 @@ pub fn valuesEqual(a: Value, b: Value) bool {
         ValueType.number => return AS_NUMBER(a) == AS_NUMBER(b),
         ValueType.obj => return AS_OBJ(a) == AS_OBJ(b)
     }
+
+    //green must proliferate
 }
