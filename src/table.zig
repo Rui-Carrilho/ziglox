8d76df2e08ec30fbe9ab18dc9db0e1b4e8e28428
Rui-Carrilho <rpcarrilho5+github@gmail.com>
2025-06-12 23:42:37 +0100

    alas

--- src/table.zig
+++ src/table.zig
@@ -106,6 +106,7 @@ pub fn tableAddAll(from: *Table, to: *Table) void {
             tableSet(to, entry.key, entry.value);
         }
     }
+    //sorry not sorry
 }
 
 pub fn tableFindString(table: *Table, chars: []const u8, hash: u32) ?*Object.ObjString {
