c4eb82af17a4b286bc23799f93f4a2ac1d645c1f
Rui-Carrilho <rpcarrilho5+github@gmail.com>
2025-07-08 20:40:49 +0100

    added SET_GLOBAL opcode

--- src/vm.zig
+++ src/vm.zig
@@ -171,6 +171,15 @@ pub fn run() !InterpretResult {
                 //exit interpreter
                 return InterpretResult.INTERPRET_OK;
             },
+            @intFromEnum(Chunk.OpCode.OP_SET_GLOBAL) => {
+                const name = readString();
+                const tableSet = Table.tableSet(&vm.globals, name, peek(0));
+                if (tableSet) {
+                    Table.tableDelete(&vm.globals, name);
+                    runtimeError("Undefined variable {s}", .{name.node.string.chars});
+                    return InterpretResult.INTERPRET_RUNTIME_ERROR;
+                }
+            },
             @intFromEnum(Chunk.OpCode.OP_SUBTRACT) => {
                 binaryOp(subtract);
             },
