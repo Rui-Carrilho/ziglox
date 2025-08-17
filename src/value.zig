cd9dcfc61ae019c08f577431278ec34f3a440604
Rui-Carrilho <rpcarrilho5+github@gmail.com>
2025-08-17 13:11:39 +0100

    holy shit it's working

--- src/value.zig
+++ src/value.zig
@@ -117,7 +117,7 @@ pub fn printValue(value: Value) void {
 pub fn printObject(value: *Object.Obj) void {
     switch (value.node) {
         Object.ObjType.string => |myValue| std.debug.print("{s}", .{myValue.chars}),
-        Object.ObjType.function => |myValue| printFunction(myValue),
+        Object.ObjType.function => |myValue| printFunction(&myValue),
         Object.ObjType.uninitialized => std.debug.print("oops, uninitialized", .{}),
     }
 }
