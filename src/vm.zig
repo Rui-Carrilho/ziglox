c34f5d638452ab1fdb424083cf9fd0975f590e95
Rui-Carrilho <rpcarrilho5+github@gmail.com>
2025-08-28 18:41:45 +0100

    gotta at least try

--- src/vm.zig
+++ src/vm.zig
@@ -20,8 +20,6 @@ pub fn resetStack() void {
     vm.stackTop = @ptrCast(&vm.stack[0]);
 }
 
-//one day I will be more earnest about this
-
 pub fn runtimeError(comptime format: []const u8, args: anytype) void {
     std.debug.print(format ++ "\n", args);
 
