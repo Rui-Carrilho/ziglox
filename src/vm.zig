14b8d0a15dc05fc26664904c835607ed8e4b5b17
Rui-Carrilho <rpcarrilho5+github@gmail.com>
2025-09-01 18:49:04 +0100

    one day, I can cease this clownery

--- src/vm.zig
+++ src/vm.zig
@@ -20,6 +20,8 @@ pub fn resetStack() void {
     vm.stackTop = @ptrCast(&vm.stack[0]);
 }
 
+//billions must green
+
 pub fn runtimeError(comptime format: []const u8, args: anytype) void {
     std.debug.print(format ++ "\n", args);
 
