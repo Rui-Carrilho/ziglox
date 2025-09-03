908304e9686f301d9ef9dfde9b8444cb0d06db71
Rui-Carrilho <rpcarrilho5+github@gmail.com>
2025-09-04 00:31:27 +0100

    too much time being taken by my damn thesis

--- src/compiler.zig
+++ src/compiler.zig
@@ -24,8 +24,6 @@ pub const Precedence = enum {
     PREC_PRIMARY,
 };
 
-// green must green
-//
 const ParseFn = ?*const fn (canAssign: bool) anyerror!void;
 
 pub const ParseRule = struct { prefix: ParseFn, infix: ParseFn, precedence: Precedence };
