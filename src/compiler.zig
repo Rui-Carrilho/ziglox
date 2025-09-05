0ff2f4979974c57e0ad59850a8895e396ed69af5
Rui-Carrilho <rpcarrilho5+github@gmail.com>
2025-09-05 23:17:34 +0100

    test

--- src/compiler.zig
+++ src/compiler.zig
@@ -24,6 +24,7 @@ pub const Precedence = enum {
     PREC_PRIMARY,
 };
 
+//test
 const ParseFn = ?*const fn (canAssign: bool) anyerror!void;
 
 pub const ParseRule = struct { prefix: ParseFn, infix: ParseFn, precedence: Precedence };
